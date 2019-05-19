1dcf06c6a93e62efd78a700105e752e34442a936
Nuno do Carmo <darthnunix@gmail.com>
2019-05-19 22:19:18 +0200

    KubeCon update
    added call to porter.yaml via URL file

--- cnab-setenv.sh
+++ cnab-setenv.sh
@@ -6,9 +6,11 @@ export DISTRO_NAME=wsldemo
 export DISTRO_PATH=/github/${DISTRO_NAME}
 export DISTRO_SOURCE=/github/${DISTRO_NAME}.tar
 export PORTER_DEP=/github/cnabwslbuilder/porter.yaml
+export PORTER_FILE=https://raw.githubusercontent.com/nunix/wslcnab/master/porter.yaml
 export DOCKER_SOURCE=docker
 export DOCKER_APP_SOURCE="/usr/bin/docker-app"
 export WSL_SOURCE=wsl.exe
 
 ### CALL PORTER ###
-porter run --action $@ --file ./porter.yaml --debug
+# porter run --action $@ --file ./porter.yaml --debug
+porter run --action $@ --file $PORTER_FILE --debug
