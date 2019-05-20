a3c06ed0ce994d384391e07bb55569135486afb2
Nuno do Carmo <darthnunix@gmail.com>
2019-05-20 20:35:18 +0200

    Removed variable for docker-app
    Removed variable for docker-app

--- cnab-setenv.sh
+++ cnab-setenv.sh
@@ -5,10 +5,8 @@ export DISTRO_IMAGE=alpine
 export DISTRO_NAME=wsldemo
 export DISTRO_PATH=/github/${DISTRO_NAME}
 export DISTRO_SOURCE=/github/${DISTRO_NAME}.tar
-export PORTER_DEP=/github/cnabwslbuilder/porter.yaml
 export PORTER_FILE=https://raw.githubusercontent.com/nunix/wslcnab/master/porter.yaml
 export DOCKER_SOURCE=docker
-export DOCKER_APP_SOURCE="/usr/bin/docker-app"
 export WSL_SOURCE=wsl.exe
 
 ### CALL PORTER ###
